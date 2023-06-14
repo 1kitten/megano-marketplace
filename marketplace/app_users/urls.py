0091bef8480eb61887ba26b5c81aacf2a246a013
Maksim Strelchenko <maksim.strelchenko99@gmail.com>
2023-06-14 20:21:23 +0300

    Delete unused import

--- marketplace/app_users/urls.py
+++ marketplace/app_users/urls.py
@@ -1,7 +1,6 @@
 from django.conf import settings
 from django.conf.urls.static import static
 from django.urls import path
-from django.contrib.auth import views as auth_views
 from .views import (AccountView, CustomLoginView, CustomLogoutView, CustomPasswordResetConfirmView,
                     CustomPasswordResetDoneView, CustomPasswordResetView,
                     CustomRegisterView, OrderDetailView, OrdersHistoryView,
